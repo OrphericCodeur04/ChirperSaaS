d78fd8b6880d5899fb951313505c7ea1e0e2b673
Orpheric ALLAGBE <florentallgbe229@gmail.com>
2023-09-28 21:19:09 +0100

    test our route and controller by sending back sms

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+//use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ChirpController extends Controller
 {
-    //
+    public function index(): Response {
+        return response('Hello World !');
+    }
 }
