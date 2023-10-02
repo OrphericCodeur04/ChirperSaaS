16507e36f58d3e377d1a049cb6bdd4827692281d
Orpheric ALLAGBE <florentallgbe229@gmail.com>
2023-10-02 14:36:34 +0100

    bind event listener to the event ChirpCreated

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Events\ChirpCreated;
+use App\Listeners\SendChirpCreatedNotifications;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -15,6 +17,12 @@ class EventServiceProvider extends ServiceProvider
      * @var array<class-string, array<int, class-string>>
      */
     protected $listen = [
+        // Enregistrement de l'Ecouteur d'Event
+        // LIER l'Ecouteur d'Event à l'Event correspondant
+        ChirpCreated::class => [
+            SendChirpCreatedNotifications::class,
+        ],
+
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
